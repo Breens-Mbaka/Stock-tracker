933268bc1963a8824e0028d0eb959bb1d58176ee
Breens-Thomas <mbakabreens@gmail.com>
2021-03-27 08:05:37 +0300

    A request to get 7 symbols to be displayed in MainActivity

--- app/src/main/java/com/moringaschool/stocktracker/networking/TwelveDataApi.java
+++ app/src/main/java/com/moringaschool/stocktracker/networking/TwelveDataApi.java
@@ -1,4 +1,12 @@
 package com.moringaschool.stocktracker.networking;
 
+import com.moringaschool.stocktracker.models.Stocks;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface TwelveDataApi {
+    @GET("quote?symbol=tsla,aapl,amzn,msft,nio,nvda,mrna&apikey=5145b6ccdac740c28ec1787fa095cdc5")
+    Call<Stocks> getStocks();
 }
