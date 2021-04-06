4df754d4fec1d893559760f93d398e1abb04924a
Breens-Thomas <mbakabreens@gmail.com>
2021-04-06 23:13:06 +0300

    Added layout for sign in and sign up

---
+++ app/src/main/java/com/moringaschool/stocktracker/ui/CreateUserAccount.java
@@ -0,0 +1,16 @@
+package com.moringaschool.stocktracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.stocktracker.R;
+
+public class CreateUserAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user_account);
+    }
+}
