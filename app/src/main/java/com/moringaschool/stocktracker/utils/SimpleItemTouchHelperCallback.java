1a122b23f487cf710d87ca3739df8530149ffd59
Breens-Thomas <mbakabreens@gmail.com>
2021-04-13 05:34:48 +0300

    Remove animations

--- app/src/main/java/com/moringaschool/stocktracker/utils/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/moringaschool/stocktracker/utils/SimpleItemTouchHelperCallback.java
@@ -55,5 +55,9 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
         super.clearView(recyclerView, viewHolder);
+        if(viewHolder instanceof ItemTouchHelperViewHolder) {
+            ItemTouchHelperViewHolder itemViewHolder = (ItemTouchHelperViewHolder) viewHolder;
+            itemViewHolder.onItemClear();
+        }
     }
 }
